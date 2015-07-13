c652bc73cf7db8c1141706d422fe77c1ab9836d2
Sasha hairov <sasha-hairov@mail.ru>
2015-07-13 20:13:37 +0300

    Разделено на 2 орудия( но добавлено только 1) изменен снаряд

--- MyTank/Assets/Game/MyTake/Scripts/raketa/Cannon Shoot.js
+++ MyTank/Assets/Game/MyTake/Scripts/raketa/Cannon Shoot.js
@@ -1,11 +1,29 @@
 var projectilPrefab : Transform;
 var powerOfShoot : float = 3000;
+var reloadTime = 0.5; 
+var ammoCount = 20; 
+private var lastShot = -10.0;
+
+
+
+
 
 function Update () {
-	if(Input.GetButtonDown("Jump")){
+
+
+
+if(Input.GetButtonDown("Jump")){
 		GetComponent.<AudioSource>().Play();
 		
 		var projectil = Instantiate(projectilPrefab, transform.position, transform.rotation);
 		projectil.GetComponent.<Rigidbody>().AddForce(transform.forward * powerOfShoot);
 	}
+	
+if (Time.time > reloadTime + lastShot && ammoCount > 0)
+	{
+	 
+	lastShot = Time.time; 
+	ammoCount-- ;
+	}
+
 }
---
+++ MyTank/Assets/Game/MyTake/Scripts/raketa/Explosion-Simple.js
@@ -0,0 +1,44 @@
+﻿var explosionRadius = 5.0; 
+var explosionPower = 10.0; 
+var explosionDamage = 100.0;
+var explosionTime = 1.0;
+
+function Start () 
+{
+	var explosionPosition = transform.position;
+	var colliders : Collider[] = Physics.OverlapSphere (explosionPosition, explosionRadius);
+	
+for (var hit in colliders) 
+	{
+		if (!hit)
+		continue;
+
+		if (hit.GetComponent.<Rigidbody>())
+		{
+			hit.GetComponent.<Rigidbody>().AddExplosionForce(
+			explosionPower, explosionPosition, explosionRadius, 3.0);
+			
+var closestPoint = hit.GetComponent.<Rigidbody>().ClosestPointOnBounds(explosionPosition); 
+			var distance = Vector3.Distance(closestPoint, explosionPosition);
+
+			// The hit points we apply fall decrease with distance from the hit point 
+var hitPoints = 1.0 - Mathf.Clamp01(distance / explosionRadius); 
+			hitPoints *= explosionDamage;
+			
+			hit.GetComponent.<Rigidbody>().SendMessageUpwards("ApplyDamage", hitPoints, 
+			SendMessageOptions.DontRequireReceiver);
+	}
+	}
+
+	// stop emitting ? 
+	if (GetComponent.<ParticleEmitter>()) 
+	{
+		GetComponent.<ParticleEmitter>().emit = true;
+		yield WaitForSeconds(0.5);
+		GetComponent.<ParticleEmitter>().emit = false;
+	}
+
+	// destroy the explosion 
+	Destroy (gameObject, explosionTime);
+}
+
